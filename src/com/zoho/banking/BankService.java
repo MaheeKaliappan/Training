1d75a31ab76c16c277f6aa305ee2f7f0d3bc39d4
maheekaliappan <maheekaliappan@gmail.com>
2022-09-27 21:26:57 +0530

    Bank Service

--- src/com/zoho/banking/BankService.java
+++ src/com/zoho/banking/BankService.java
@@ -4,17 +4,15 @@ import java.util.List;
 
 public class BankService {
 
-	 static boolean findUser(List<Bank> bankDetails, int userIdNo, String userName) {
-		for (int index = 0; index < bankDetails.size(); index++) {
-			if ((bankDetails.get(index).getCustomerId() == userIdNo)
-					&& (bankDetails.get(index).getCustomerName().equals(userName))) {
-				return true;
-			}
+	static boolean findUser(List<Bank> bankDetails, int userIdNo, String userName) {
+		if ((bankDetails.get(userIdNo - 1).getCustomerId() == userIdNo)
+				&& (bankDetails.get(userIdNo - 1).getCustomerName().equals(userName))) {
+			return true;
 		}
 		return false;
 	}
 
-	 static void showAdminPage(List<Bank> bankDetails) {
+	static void showAdminPage(List<Bank> bankDetails) {
 		if (bankDetails.size() != 0) {
 			System.out.println("Over All User Details");
 			for (int index = 0; index < bankDetails.size(); index++) {
@@ -35,57 +33,51 @@ public class BankService {
 
 	static void viewDetails(int userIdNo, List<Bank> bankDetails) {
 		System.out.println("Your Details");
-		for (int index = 0; index < bankDetails.size(); index++) {
-			if (bankDetails.get(index).getCustomerId() == userIdNo) {
-				System.out.println("Ur Id                : " + bankDetails.get(index).getCustomerId());
-				System.out.println("Ur Name              : " + bankDetails.get(index).getCustomerName());
-				System.out.println("Ur Account Number    : " + bankDetails.get(index).getCustomerAccountNumber());
-				System.out.println("Ur Previous Balance  : " + bankDetails.get(index).getCustomerPreviousBalance());
-				System.out.println("Ur Current Balance   : " + bankDetails.get(index).getCustomerCurrentBalance());
-				System.out.println("Ur Withdraw Amount   : " + bankDetails.get(index).getCustomerWithdrawAmount());
-				break;
-			}
-		}
+		if (bankDetails.get(userIdNo - 1).getCustomerId() == userIdNo) {
+			System.out.println("Ur Id                : " + bankDetails.get(userIdNo - 1).getCustomerId());
+			System.out.println("Ur Name              : " + bankDetails.get(userIdNo - 1).getCustomerName());
+			System.out.println("Ur Account Number    : " + bankDetails.get(userIdNo - 1).getCustomerAccountNumber());
+			System.out.println("Ur Previous Balance  : " + bankDetails.get(userIdNo - 1).getCustomerPreviousBalance());
+			System.out.println("Ur Current Balance   : " + bankDetails.get(userIdNo - 1).getCustomerCurrentBalance());
+			System.out.println("Ur Withdraw Amount   : " + bankDetails.get(userIdNo - 1).getCustomerWithdrawAmount());
 
+		}
 	}
 
-	 static void withdraw(int withdraw, int userIdNo, List<Bank> bankDetails) {
-		for (int index = 0; index < bankDetails.size(); index++) {
-			if (bankDetails.get(index).getCustomerId() == userIdNo) {
-				if (bankDetails.get(index).getCustomerCurrentBalance() >= withdraw) {
-					bankDetails.get(index)
-							.setCustomerPreviousBalance(bankDetails.get(index).getCustomerCurrentBalance());
-					int withdrawAmount = bankDetails.get(index).getCustomerCurrentBalance() - withdraw;
-					if (bankDetails.get(index).getCustomerWithdrawAmount() != null) {
-						int totalWithDraw = bankDetails.get(index).getCustomerWithdrawAmount() + withdrawAmount;
-						bankDetails.get(index).setCustomerWithdrawAmount(totalWithDraw);
-					} else {
-						bankDetails.get(index).setCustomerWithdrawAmount(withdraw);
-					}
+	static void withdraw(int withdraw, int userIdNo, List<Bank> bankDetails) {
 
-					bankDetails.get(index).setCustomerCurrentBalance(withdrawAmount);
-					System.out.println("Withdraw Successfully");
-					break;
+		if (bankDetails.get(userIdNo - 1).getCustomerId() == userIdNo) {
+			if (bankDetails.get(userIdNo - 1).getCustomerCurrentBalance() >= withdraw) {
+				bankDetails.get(userIdNo - 1)
+						.setCustomerPreviousBalance(bankDetails.get(userIdNo - 1).getCustomerCurrentBalance());
+				int withdrawAmount = bankDetails.get(userIdNo - 1).getCustomerCurrentBalance() - withdraw;
+				if (bankDetails.get(userIdNo - 1).getCustomerWithdrawAmount() != null) {
+					int totalWithDraw = bankDetails.get(userIdNo - 1).getCustomerWithdrawAmount() + withdraw;
+					bankDetails.get(userIdNo - 1).setCustomerWithdrawAmount(totalWithDraw);
 				} else {
-					System.out.println("Insufficient Balance");
+					bankDetails.get(userIdNo - 1).setCustomerWithdrawAmount(withdraw);
 				}
+
+				bankDetails.get(userIdNo - 1).setCustomerCurrentBalance(withdrawAmount);
+				System.out.println("Withdraw Successfully");
+
+			} else {
+				System.out.println("Insufficient Balance");
 			}
 		}
-
 	}
 
-	 static void depositAmount(int amount, int userIdNo, List<Bank> bankDetails) {
+	static void depositAmount(int amount, int userIdNo, List<Bank> bankDetails) {
 		int totalAmount = 0;
-		for (int index = 0; index < bankDetails.size(); index++) {
-			if (bankDetails.get(index).getCustomerId() == userIdNo) {
-				bankDetails.get(index).setCustomerPreviousBalance(bankDetails.get(index).getCustomerCurrentBalance());
-				totalAmount = amount + bankDetails.get(index).getCustomerCurrentBalance();
-				bankDetails.get(index).setCustomerCurrentBalance(totalAmount);
-				System.out.println("Deposit Successfully");
-				break;
-			}
+
+		if (bankDetails.get(userIdNo - 1).getCustomerId() == userIdNo) {
+			bankDetails.get(userIdNo - 1)
+					.setCustomerPreviousBalance(bankDetails.get(userIdNo - 1).getCustomerCurrentBalance());
+			totalAmount = amount + bankDetails.get(userIdNo - 1).getCustomerCurrentBalance();
+			bankDetails.get(userIdNo - 1).setCustomerCurrentBalance(totalAmount);
+			System.out.println("Deposit Successfully");
+
 		}
-
-
 	}
+
 }
